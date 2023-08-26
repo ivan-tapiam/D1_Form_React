bd8bc102e98ab0ab2fc374655b19b24654ae28e1
ivan-tapiam <ivan.tapiam@mayor.cl>
2023-08-25 21:04:48 -0400

    Feo, pero funcional

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -18,40 +18,49 @@ const Formulario = () => {
       return
     }
   setError(false)
+  setNombre('');
+  setEmail('');
+  setPassword('');
+  setPassword2('');
   }
 
   return (
     <>
     <Form onSubmit={validarInputs}>
-      {error ? <p className='error'>Debes inrgesar tu nombre</p> : null}
       
       <p>O usa tu email para registrarte </p>
       <Form.Group className="mb-3" controlId="formBasicName">
         <input className='Form.Control' type="name" placeholder="Nombre" 
-        onChange={(e) => setNombre(e.target.value)}/>
+        onChange={(e) => setNombre(e.target.value)}
+        value={nombre}/>
       </Form.Group>
       
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <input className='Form.Control' type="email" placeholder="Tuemail@ejemplo.com" 
-        onChange={(e) => setEmail(e.target.value)}/>
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <input className='Form.Control' type="password" placeholder="Contraseña" 
-        onChange={(e) => setPassword(e.target.value)}/>
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}/>
       </Form.Group>
       
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <input className='Form.Control' type="password" placeholder="Confirma tu contraseña" 
-        onChange={(e) => setPassword2(e.target.value)}/>
+        onChange={(e) => setPassword2(e.target.value)}
+        value={password2}/>
       </Form.Group>
    
+   <br />
     <div className="d-grid gap-2">
       <Button variant="success" size="lg" type='submit'>
         Registrarse
       </Button>
     </div>
     </Form>
+    {error ? <p className='error'>Debes ingresar todos los campos</p> : null}
     </>
   )
 }
